e08eafb23974b9a1da8ca601d214d73b0d8e67c7
mixmix19892@gmail.com <gnork666@mail.ru>
2026-08-03 23:58:14 +0300

    Блогикум финал

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('category/<slug:category_slug>/',
          views.category_posts,
          name='category_posts'),
-         
 ]
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def about(request):
-    template = 'about.html'
+    template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
-    template = 'rules.html'
+    template = 'pages/rules.html'
     return render(request, template)
